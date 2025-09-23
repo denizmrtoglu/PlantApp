4af3ba3b970492761e4342605b22794e065a6606
Deniz Muratoğlu <denizmrtoglu@hotmail.com>
2025-09-23 12:57:18 +0300

    Feature/PA-2 Implemented tab layout (#1)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,17 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import {
+  DiagnoseIcon,
+  GardenIcon,
+  HomeIcon,
+  ProfileIcon,
+  ScanIcon,
+} from "@/assets/icons";
+import { HapticTab } from "@/components/haptic-tab";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { StyleSheet, View } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -12,24 +19,81 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarInactiveTintColor: Colors[colorScheme ?? "light"].tabIconDefault,
         headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+        tabBarButton: (props) => (
+          <HapticTab {...props} style={styles.tabBarButton} />
+        ),
+        tabBarLabelStyle: {
+          fontFamily: "Rubik_400Regular",
+          fontWeight: "400",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ color }) => <HomeIcon size={25} color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="diagnose"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Diagnose",
+          tabBarIcon: ({ color }) => <DiagnoseIcon size={25} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="scan"
+        options={{
+          title: "Scan",
+          tabBarLabelStyle: {
+            display: "none",
+          },
+          tabBarIcon: () => (
+            <View style={styles.scanIcon}>
+              <ScanIcon size={25} color={"white"} />
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="my-garden"
+        options={{
+          title: "My Garden",
+          tabBarIcon: ({ color }) => <GardenIcon size={25} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => <ProfileIcon size={25} color={color} />,
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBarButton: {
+    paddingTop: 6,
+    paddingBottom: 1,
+    height: 49,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  scanIcon: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "#2CCC80",
+    borderWidth: 4,
+    marginBottom: 22,
+    backgroundColor: Colors.light.tint,
+  },
+});
---
+++ assets/icons/index.tsx
@@ -0,0 +1,6 @@
+export { default as DiagnoseIcon } from './diagnose';
+export { default as GardenIcon } from './garden';
+export { default as HomeIcon } from './home';
+export { default as ProfileIcon } from './profile';
+export { default as ScanIcon } from './scan';
+
