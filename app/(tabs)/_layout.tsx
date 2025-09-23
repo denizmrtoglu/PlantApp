6f6b19c033082be5eea1cdbde44757f16defe3e0
Deniz Muratoğlu <deniz.muratoglu@creainc.us>
2025-09-23 12:01:03 +0300

    Implemented tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,11 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
+import { DiagnoseIcon, GardenIcon, HomeIcon, ProfileIcon, ScanIcon } from '@/assets/icons';
 import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { View } from 'react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -13,6 +14,7 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarInactiveTintColor: Colors[colorScheme ?? 'light'].tabIconDefault,
         headerShown: false,
         tabBarButton: HapticTab,
       }}>
@@ -20,14 +22,40 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <HomeIcon size={25} color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="diagnose"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Diagnose',
+          tabBarIcon: ({ color }) => <DiagnoseIcon size={25} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="scan"
+        options={{
+          title: 'Scan',
+          tabBarLabelStyle: {
+            display: 'none',
+          },
+          tabBarIcon: () => <View style={{ width: 64, height: 64, borderRadius: 32, alignItems: 'center', justifyContent: 'center', borderColor: "#2CCC80", borderWidth: 4, marginBottom: 8, backgroundColor: Colors[colorScheme ?? 'light'].tint }}>
+            <ScanIcon size={25} color={"white"} />
+          </View>,
+        }}
+      />
+      <Tabs.Screen
+        name="my-garden"
+        options={{
+          title: 'My Garden',
+          tabBarIcon: ({ color }) => <GardenIcon size={25} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <ProfileIcon size={25} color={color} />,
         }}
       />
     </Tabs>
